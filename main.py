82265d15baa62beacaccef0e3bd59af344bfe31e
polop4793-collab <polop4793@gmail.com>
2026-03-18 13:57:30 -0500

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+user = "polo9347@gmail.com"
+password = "poloperez9347*"
+
+user_input = input()
+password_input = input()
+
+if user_input == user and password_input == password:
+    print("Sesión exitosa")
+else:
+    print("Contraseña o usuario incorrecta")
